9e108d4bb3538672c73b1a77f75154a48502cb5e
afranco <adrianfrancof@gmail.com>
2022-09-07 08:38:01 -0300

    changes at main.dart import new package

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+// This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Welcome to Flutter',
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Welcome to Flutter')
+          ),
+          body: const Center(
+            child: Text('Hello Word'),
+          ),
+        )
+    );
+  }
+}
