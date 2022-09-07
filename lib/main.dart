45971709495ceb948224cf55012c0a29222a26fe
afranco <adrianfrancof@gmail.com>
2022-09-07 09:15:57 -0300

    Create an infinite scrolling ListView

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,12 @@ import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
+//principal method
 void main() {
   runApp(const MyApp());
 }
 
+//MyApp stateless widget.
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -17,30 +19,46 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Welcome to Flutter',
+        title: 'Startup Name Generator',
         home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Welcome to Flutter')
-          ),
+          appBar: AppBar(title: const Text('Startup Name Generator')),
           body: const Center(
             child: RandomWords(),
           ),
-        )
+        ));
+  }
+}
+
+//stateful widget
+//Stateless widgets are immutable, meaning that their properties can’t change—all values are final.
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: /*1*/ (context, i) {
+        if (i.isOdd) return const Divider(); /*2*/
+
+        final index = i ~/ 2; /*3*/
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+        }
+        return ListTile(
+          title: Text(
+            _suggestions[index].asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      },
     );
   }
 }
 
 class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
-
+  const RandomWords({super.key});
   @override
   State<RandomWords> createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  @override
-  Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
-  }
 }
